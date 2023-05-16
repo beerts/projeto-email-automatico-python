df3e2cb17ea6444464ae4ed8356915f3cd5a3b6d
beerts <133691749+beerts@users.noreply.github.com>
2023-05-16 14:18:14 -0300

    Update Projeto1 - Automatizando e-mail (INTENSIVAO HASHTAG).py

--- Projeto1 - Automatizando e-mail (INTENSIVAO HASHTAG).py
+++ Projeto1 - Automatizando e-mail (INTENSIVAO HASHTAG).py
@@ -3,8 +3,6 @@
 
 # # Automação de Sistemas e Processos com Python
 # 
-# ### Desafio:
-# 
 # Para controle de custos, todos os dias, seu chefe pede um relatório com todas as compras de mercadorias da empresa.
 # O seu trabalho, como analista, é enviar um e-mail para ele, assim que começar a trabalhar, com o total gasto, a quantidade de produtos compradas e o preço médio dos produtos.
 # 
